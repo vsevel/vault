c8dd536095447ca580a8086a022b5b17193aec8b
Vincent Sevel <vsevel@macbook-de-vincent.home>
2019-01-26 06:00:21 +0100

    lab3

--- lab3/create-all.sh
+++ lab3/create-all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-helm install --name consul ../../consul-helm/
-kubectl patch sts consul-server --type json -p='[{"op": "remove", "path": "/spec/template/spec/affinity"}]'
-kubectl delete pods -l component=server
+# helm install --name consul ../../consul-helm/
+# kubectl patch sts consul-server --type json -p='[{"op": "remove", "path": "/spec/template/spec/affinity"}]'
+# kubectl delete pods -l component=server
 
 # kubectl create configmap vault-config --from-file=vault-config.json
-# source create-cert.sh
-# kubectl apply -f vault-pvc.yaml
-# kubectl apply -f vault-deployment.yaml
-# kubectl apply -f vault-service.yaml
-# kubectl apply -f vault-ingress.yaml
+source create-cert.sh
+kubectl apply -f vault-pvc.yaml
+kubectl apply -f vault-deployment.yaml
+kubectl apply -f vault-service.yaml
+kubectl apply -f vault-ingress.yaml
 
--- lab3/remove-all.sh
+++ lab3/remove-all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-helm delete $(helm ls --short) --purge
+# helm delete $(helm ls --short) --purge
 
-# rm -f work/*
-# kubectl delete ingress --all
-# kubectl delete svc --all
-# kubectl delete deployment --all
-# kubectl delete secret --all
-# kubectl delete csr --all
+rm -f work/*
+kubectl delete ingress --all
+kubectl delete svc --all
+kubectl delete deployment --all
+kubectl delete secret --all
+kubectl delete csr --all
 # kubectl delete cm --all
-# kubectl delete pvc --all
+kubectl delete pvc --all
 
 
