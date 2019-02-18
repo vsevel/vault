3e85a2d899ef232d95ca7a15a2efdaebb1f6723f
Vincent Sevel <vincent@sevel.eu>
2019-02-18 20:55:00 +0100

    step8

--- vault-demo/build_docker.sh
+++ vault-demo/build_docker.sh
@@ -7,5 +7,5 @@ mkdir target/docker
 cp src/main/docker/* target/docker
 cp target/vault-demo-0.0.1-SNAPSHOT.jar target/docker
 docker build target/docker -t vault-demo
-kubectl delete -f vault-demo.yaml
-kubectl apply -f vault-demo.yaml
+#kubectl delete -f vault-demo.yaml
+#kubectl apply -f vault-demo.yaml
