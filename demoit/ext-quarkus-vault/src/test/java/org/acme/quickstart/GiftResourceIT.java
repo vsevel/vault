138677eed7faa0f75e160e87907584dba23c8b0f
Vincent Sevel <vincent@sevel.eu>
2019-04-27 21:25:51 +0200

    quarkusapp

--- demoit/ext-quarkus-vault/src/test/java/org/acme/quickstart/GiftResourceIT.java
+++ demoit/ext-quarkus-vault/src/test/java/org/acme/quickstart/GiftResourceIT.java
@@ -10,7 +10,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GiftResourceTest {
+public class GiftResourceIT {
 
     @Test
     public void testGiftEndpoint() {
--- demoit/ext-quarkus-vault/src/test/java/org/acme/quickstart/NativeGreetingResourceIT.java
+++ demoit/ext-quarkus-vault/src/test/java/org/acme/quickstart/NativeGreetingResourceIT.java
@@ -3,7 +3,7 @@ package org.acme.quickstart;
 import io.quarkus.test.junit.SubstrateTest;
 
 @SubstrateTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeGreetingResourceIT extends GreetingResourceIT {
 
     // Execute the same tests but in native mode.
 }
